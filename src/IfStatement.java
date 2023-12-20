d91f4a0f16f121eb672f6705057bcda8bd7151ed
Rubí <rubi_salcedocas@outlook.es>
2023-12-20 03:07:15 -0700

    New

---
+++ src/IfStatement.java
@@ -0,0 +1,21 @@
+public class IfStatement {
+    public static void main(String[] arg) {
+
+        /*bolean isBluetoothEnabled = true;
+        int fileSended = 3;
+
+        if (isBluetoothEnabled){
+            //Send file
+            fileSended++;
+            System.out.println("Archivo Enviado");
+            int i = 0;
+            i ++;
+        }
+        else {
+            System.out.println("Por favor enciende tu Bluetooth");
+        }
+
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSended);*/
+    }  
+}
